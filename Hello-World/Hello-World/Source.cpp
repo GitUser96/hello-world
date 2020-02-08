02496721976665b4d144582c0608164eb93cb468
Mai Elshazly <mai_swu.1996@yahoo.com>
2020-02-09 00:40:32 +0200

    Message 4.

--- Hello-World/Hello-World/Source.cpp
+++ Hello-World/Hello-World/Source.cpp
@@ -1,7 +1,6 @@
-//#include <iostream>
-//using namespace std;
-//void main()
-//{
-//	cout << "Hello World!!" << endl;
-//	cout << "Thankful." << endl;
-//}
+#include <iostream>
+using namespace std;
+void main()
+{
+	cout << "Hello World!!" << endl;
+}
